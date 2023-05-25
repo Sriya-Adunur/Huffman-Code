bb02354b610227302bd3eab119c2033aa5745588
Sriya-Adunur <sadunur@calpoly.edu>
2023-05-24 20:26:27 -0700

    Comprehensive tests for program implementing Huffman coding.

--- huffman_tests.py
+++ huffman_tests.py
@@ -93,23 +93,48 @@ class TestList(unittest.TestCase):
         huffman_encode("file4.txt", "file4compare.txt")
         self.assertTrue(compare_files("file4.txt", "file4compare.txt"))
 
+    def test_parse_header(self) -> None:
+      header = "97 2 98 4 99 8 100 16 102 2"
+      freqlist = parse_header(header)
+      anslist = [0]*256
+      anslist[97:104] = [2, 4, 8, 16, 0, 2, 0]
+      self.assertListEqual(freqlist[97:104], anslist[97:104])
 
+    def test_parse_header2(self) -> None:
+       header = "65 3 66 1 67 5 68 2"
+       freqlist = parse_header(header)
+       anslist = [0] * 256
+       anslist[65:69] = [3, 1, 5, 2]
+       self.assertListEqual(freqlist[65:69], anslist[65:69])
 
-# Compare files - takes care of CR/LF, LF issues
+    def test_decode_01(self) -> None:
+      huffman_decode("file1_soln.txt", "file1_decode.txt")
+      # detect errors by comparing your encoded file with a *known* solution file
+      self.assertTrue(compare_files("file1.txt", "file1_decode.txt"))
+
+    def test_decode_02(self) -> None:
+      huffman_decode("declaration_soln.txt", "declaration_decode.txt")
+      # detect errors by comparing your encoded file with a *known* solution file
+      self.assertTrue(compare_files("declaration.txt", "declaration_decode.txt"))
+
+    def test_error3_textfile(self) -> None:
+       with self.assertRaises(FileNotFoundError):  # uses context manager for checking exception
+           huffman_decode("file7_soln.txt", "file1_decode.txt")
+
 def compare_files(file1: str, file2: str) -> bool: # pragma: no cover
-    match = True
-    done = False
-    with open(file1, "r") as f1:
-        with open(file2, "r") as f2:
-            while not done:
-                line1 = f1.readline().strip()
-                line2 = f2.readline().strip()
-                if line1 == '' and line2 == '':
-                    done = True
-                if line1 != line2:
-                    done = True
-                    match = False
-    return match
+        match = True
+        done = False
+        with open(file1, "r") as f1:
+            with open(file2, "r") as f2:
+                while not done:
+                    line1 = f1.readline().strip()
+                    line2 = f2.readline().strip()
+                    if line1 == '' and line2 == '':
+                        done = True
+                    if line1 != line2:
+                        done = True
+                        match = False
+        return match
 
 
 if __name__ == '__main__':
